e94abad3ce2fcde044cc8c45038fbbcea36ace4c
philip <philip.clarke@redhood.co.uk>
2016-01-14 14:52:40 +0000

    added protractor

---
+++ test/e2e/getspuds.spec.js
@@ -0,0 +1,9 @@
+describe('searching for spuds', (){
+  it('has a title', function() {
+    browser.get('http://127.0.0.1:8080/');
+
+    expect(browser.getTitle()).toEqual('Spud Search');
+  });
+});
+
+})
