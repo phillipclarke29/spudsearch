b412d4be2e90b4f2d3b7607794665b9bd891b02f
philip <philip.clarke@redhood.co.uk>
2016-01-14 15:00:44 +0000

    protractor working and test passing

--- test/e2e/getspuds.spec.js
+++ test/e2e/getspuds.spec.js
@@ -1,9 +1,7 @@
-describe('searching for spuds', (){
+describe('searching for spuds', function(){
   it('has a title', function() {
-    browser.get('http://127.0.0.1:8080/');
+    browser.get('http://127.0.0.1:8080');
 
     expect(browser.getTitle()).toEqual('Spud Search');
   });
 });
-
-})
