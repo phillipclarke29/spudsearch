1cbf279bae4cf780c3201036b9b25205cff31410
philip <philip.clarke@redhood.co.uk>
2016-01-19 18:21:57 +0000

    added name of user as link to flickr profile

--- ng/filters.js
+++ ng/filters.js
@@ -5,3 +5,11 @@ app.filter('authorLink', function() {
     return out[1];
   };
 })
+
+app.filter('authorName', function() {
+  return function(input) {
+    var out = input.match(/https:\/\/www.flickr.com\/people\/([\s\S]*?)\/"/);
+
+    return out[1];
+  };
+})
