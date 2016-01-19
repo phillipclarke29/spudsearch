682133214e2991be86e0fddb2fb08381aa8e7592
philip <philip.clarke@redhood.co.uk>
2016-01-19 11:11:51 +0000

    altered filter

--- ng/filters.js
+++ ng/filters.js
@@ -1,6 +1,6 @@
 app.filter('test', function() {
   return function(input) {
-    var out = input.match(/<a[^>]*>([\s\S]*?)<\/a>/);
+    var out = input.match(/<a[^>]*>([\s\S]*?)<\/a>/).join();
 
     return out;
   };
