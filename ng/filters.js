f657bfa803ab98634df58c599bef03d2170e606f
philip <philip.clarke@redhood.co.uk>
2016-01-20 08:30:53 +0000

    descritption added to modal

--- ng/filters.js
+++ ng/filters.js
@@ -13,3 +13,10 @@ app.filter('authorName', function() {
     return out[1];
   };
 })
+app.filter('description', function() {
+  return function(input) {
+    var out = input.match(/\/><\/a><\/p> <p>([\s\S]*?)<\/p>/);
+
+    return out[1];
+  };
+})
